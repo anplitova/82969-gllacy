4e52b3f3b08fdf4f785bcd04b586d0d0d4931311
Anplitova Ekaterina <anplitova@yandex.ru>
2017-02-16 01:50:23 +0300

    Доработка верстки. Главная. Каталог.

--- js/script.min.js
+++ js/script.min.js
@@ -35,8 +35,13 @@ function initSlider() {
       var activeSlide = document.querySelector('.js-slider-item.m-show');
       if (activeSlide) {
         activeSlide.classList.remove('m-show');
+        for (var i = 0; i < body.classList.length; i++) {
+          if (body.classList[i].indexOf('m-slide') !== -1) {
+            body.classList.remove(body.classList[i]);
+          }
+        }
       }
-      body.style.backgroundColor = data.color;
+      body.classList.add('m-slide-' + data.slide);
       sliderList[data.slide - 1].classList.add('m-show');
     };
   });
@@ -46,23 +51,34 @@ function initModal() {
   var openModalButton = document.querySelector('.js-open-modal');
   var closeModalButton = document.querySelector('.js-close-modal');
   var overlayModal = document.querySelector('.js-overlay-modal');
+  var openModal;
 
   openModalButton.onclick = function (e) {
-    event.preventDefault();
+    e.preventDefault();
     var data = e.target.dataset;
     var modal = document.getElementById(data.modal);
     modal.classList.add('m-show');
   };
 
-  closeModalButton.onclick = function () {
-    var openModal = document.querySelector('.js-modal.m-show');
+  closeModalButton.onclick = function (e) {
+    e.preventDefault();
+    openModal = document.querySelector('.js-modal.m-show');
     openModal.classList.remove('m-show');
   };
 
   overlayModal.onclick = function (e) {
     if (e.target.classList.contains('js-overlay-modal')) {
-      var openModal = document.querySelector('.js-modal.m-show');
+      openModal = document.querySelector('.js-modal.m-show');
       openModal.classList.remove('m-show');
     }
   };
+
+  window.onkeydown = function (e) {
+    if (e.keyCode === 27) {
+      openModal = document.querySelector('.js-modal.m-show');
+      if (openModal) {
+        openModal.classList.remove('m-show');
+      }
+    }
+  }
 }
